38acec24113247e3101f1d11c3a96d780d6842ca
Konstantine Ghudushauri <kote.ghudushauri@gmail.com>
2022-06-21 19:59:19 +0400

    this is third commit

--- home-project/ilovecoffee/src/coffees/coffees.controller.ts
+++ home-project/ilovecoffee/src/coffees/coffees.controller.ts
@@ -8,30 +8,32 @@ import {
   Post,
   Query,
 } from '@nestjs/common';
+import { CoffeesService } from './coffees.service';
 
 @Controller('coffees')
 export class CoffeesController {
+  constructor(private readonly cofffeesService: CoffeesService) {}
   @Get()
   findAll(@Query() paginationQuery) {
-    const { limit, offset } = paginationQuery;
-    return `This action returns all coffees Limit: ${limit}, offset: ${offset}`;
+    //const { limit, offset } = paginationQuery;
+    return this.cofffeesService.findAll();
   }
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return `This action returns #${id} coffees`;
+    return this.cofffeesService.findOne(id);
   }
   @Post()
   create(@Body() body) {
-    return body;
+    return this.cofffeesService.create(body);
   }
 
   @Patch(':id')
-  update(@Param('id') id: String, @Body() body) {
-    return `This action updates #${id} coffe`;
+  update(@Param('id') id: string, @Body() body) {
+    return this.cofffeesService.update(id, body);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return `This action removes #${id} coffe`;
+    return this.cofffeesService.remove(id);
   }
 }
